0181405dfe87992cd4e1b04f5704f456391eeb45
VincECodesNicely <vk6953@hotmail.com>
2025-08-02 13:15:59 +1200

    chain calculations work

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const container = document.querySelector(".container");
 const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll("[data-num]");
-
-numberButtons.forEach((element) => {
-  element.addEventListener("click", () => {
-    display.textContent += element.textContent;
-  });
-});
-
-document.querySelector("#AC").addEventListener("click", () => {
-  display.textContent = "";
-});
+const operandButtons = document.querySelectorAll("#operand");
+const equals = document.querySelector("#equal");
+let operand = "";
+let numOne;
+let numTwo;
+let currentInput = "";
 
 function displayNumber(number) {
   display.textContent += number;
@@ -31,3 +27,70 @@ function multiplication(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(numOne, operand, numTwo) {
+  let result;
+  switch (operand) {
+    case "+":
+      result = addition(numOne, numTwo);
+      break;
+    case "-":
+      result = subtraction(numOne, numTwo);
+      break;
+    case "x":
+      result = multiplication(numOne, numTwo);
+      break;
+    case "÷":
+      result = divide(numOne, numTwo);
+      break;
+  }
+  return result;
+}
+
+numberButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    currentInput += element.textContent;
+    display.textContent += element.textContent;
+  });
+});
+
+operandButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (currentInput === "") return;
+
+    if (numOne !== undefined && operand !== null) {
+      numTwo = parseFloat(currentInput);
+      console.log("n1:", numOne);
+      console.log("n2:", numTwo);
+      console.log("operand:", operand);
+
+      const result = operate(numOne, operand, numTwo);
+      numOne = result;
+      display.textContent = result + " " + element.textContent + " ";
+    } else {
+      numOne = parseFloat(currentInput);
+      display.textContent += " " + element.textContent + " ";
+    }
+
+    operand = element.textContent;
+    currentInput = "";
+  });
+});
+
+document.querySelector("#AC").addEventListener("click", () => {
+  numOne = undefined;
+  operand = null;
+  currentInput = "";
+  display.textContent = "";
+});
+
+equals.addEventListener("click", () => {
+  if (currentInput === "") return;
+  numTwo = parseFloat(currentInput);
+  const result = operate(numOne, operand, numTwo);
+  display.textContent = result;
+  currentInput = result;
+  numOne = null;
+  numTwo = null;
+  operand = null;
+});
